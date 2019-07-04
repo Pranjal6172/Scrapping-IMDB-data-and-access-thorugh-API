84dad40df54d9fade77ca6c1c7421f6aceb5de48
Pranjal Mehta <38372934+Pranjal6172@users.noreply.github.com>
2019-07-04 14:32:13 +0530

    Update FlaskAPI.py
    both the action for api is added

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, request
 import pymongo as pm
 
@@ -36,3 +38,48 @@ def api2():
     for record in cursor: 
         d.append(record["Index"]) 
     return render_template("index2.html",data=d)                               #another page for api2
+
+@app.route("/movies",methods=["POST","GET"])                                       #this is action for api2
+def movies():
+    if request.method=="POST":
+        name_id=request.form['idx']
+    try: 
+        conn = pm.MongoClient() 
+        print("Connected successfully!!!") 
+    except:   
+        print("Could not connect to MongoDB") 
+      
+    # database 
+    db = conn.database
+    collection = db.IMDB_Collection
+    #print(name_id,"check")
+    cursor = collection.find({"Index":int(name_id)})
+    movie_string=""
+    for i in cursor:
+        movie_string=movie_string+str(i["Index"])+"."+"NAME:-"+" "+i["Movie_name"]+" "+str(i["Year"])+" || "+"Cast :"+",".join(i["Star_Cast"])+" || "+"RATINGS:-"+" "+str(i["Ratings"])
+    return movie_string                                                    #returnng string that contains desirable information
+
+@app.route("/movie",methods=["POST","GET"])                                   #action for api1
+def movie():
+    if request.method=="POST":
+        moviename=request.form['movie_name']
+    try: 
+        conn = pm.MongoClient() 
+        print("Connected successfully!!!") 
+    except:   
+        print("Could not connect to MongoDB") 
+      
+    # database 
+    db = conn.database
+    collection = db.IMDB_Collection
+    #print(moviename,"check")
+    cursor = collection.find({"Movie_name":moviename})
+    movies_str=""
+    for j in cursor:
+        movies_str=movies_str+str(j["Index"])+"."+"NAME:-"+" "+j["Movie_name"]+" "+str(j["Year"])+" || "+"Cast :"+",".join(j["Star_Cast"])+" || "+"RATINGS:-"+" "+str(j["Ratings"])
+    return movies_str                                                     #returning string that contains desirable information
+    
+                
+if __name__=='__main__':
+    app.run()
+    
