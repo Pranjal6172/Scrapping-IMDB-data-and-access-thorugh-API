7f06e71418bc8e74b0acf60b0fbad7b9fed781ab
Pranjal Mehta <38372934+Pranjal6172@users.noreply.github.com>
2019-07-04 14:27:47 +0530

    Update FlaskAPI.py
    api 2 is added

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template, url_for, request, session, redirect, jsonify
+from flask import Flask, render_template, request
 import pymongo as pm
 
 app = Flask(__name__)
-@app.route('/')
+@app.route('/api1')                                       #api for getting movie name
 def api1():
+    try: 
+        conn = pm.MongoClient()                          #connecting to mongodb
+        print("Connected successfully!!!") 
+    except:   
+        print("Could not connect to MongoDB") 
+      
+    # database 
+    db = conn.database
+    collection = db.IMDB_Collection
+    cursor = collection.find()
+    d=[]
+    for record in cursor: 
+        d.append(record["Movie_name"]) 
+    #print(d)
+    return render_template("index.html",data=d)                   #render data
+
+@app.route('/api2')                                                #api for getting movie detail through index
+def api2():
     try: 
         conn = pm.MongoClient() 
         print("Connected successfully!!!") 
@@ -16,8 +34,5 @@ def api1():
     cursor = collection.find()
     d=[]
     for record in cursor: 
-        d.append(record["Movie_name"]) 
-    return render_template("index.html",data=d)
-    
-if __name__=='__main__':
-    app.run()
+        d.append(record["Index"]) 
+    return render_template("index2.html",data=d)                               #another page for api2
